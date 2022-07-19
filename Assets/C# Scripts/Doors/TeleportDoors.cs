3e016d1bb7c33551df6124d448f4bbc713047f56
DamithTennakoon <88393680+DamithTennakoon@users.noreply.github.com>
2022-07-19 13:29:08 -0400

    Update character controller positions

--- Assets/C# Scripts/Doors/TeleportDoors.cs
+++ Assets/C# Scripts/Doors/TeleportDoors.cs
@@ -12,47 +12,85 @@ using UnityEngine;
 
 public class TeleportDoors : MonoBehaviour
 {
-    // Define integer variables
-    public float MaxDisplacement;
-    public float DoorAPos;
-    public float DoorBPos;
-    public float StartDoorA;
-    public float StartDoorB;
-
     // Define Game Objects for Door's A and B
     public GameObject DoorA;
     public GameObject DoorB;
 
+    // Define Game Object for Sensor
+    public Collider Sensor;
+
+    // Define Boolean to detect player interaction
+    public bool PlayerDetected;
+
+    // Define Boolean for sate of door
+    public bool Opening;
+
     // NOTE: Max displacement = 1.698 +/- Z
+    // Define variable for maximum door opening distance
+    public float MaxOpenDistance = 1.698f;
+
+    // Define variables to get inititial position of doors
+    public Vector3 DoorAPos;
+    public Vector3 DoorBPos;
+
+    // Define varible to control door open speed
+    public float DoorOpenSpeed = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Inistantiate public integer variables
-        MaxDisplacement = 1.698f;
-
-        // Instantiate initiatial position of doors
-        StartDoorA = DoorA.transform.position.z;
-        StartDoorB = DoorB.transform.position.z;
-
+        // Instantiate initiate position of doors to Vector3 variables
+        DoorAPos = DoorA.transform.position;
+        DoorBPos = DoorB.transform.position;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Check to see if player is here or not
+        SensorOnTrigger(Sensor);
+        SensorOffTrigger(Sensor);
+
+        // Open the door if the player is detected and if door is not fully open
+        if (PlayerDetected)
+        {
+            if (DoorA.transform.position.z < DoorAPos[2] + MaxOpenDistance)
+            {
+                if (DoorB.transform.position.z > DoorBPos[2] - MaxOpenDistance)
+                {
+                    DoorA.transform.Translate(DoorAPos[0], DoorAPos[1], DoorOpenSpeed * Time.deltaTime);
+                    DoorB.transform.Translate(DoorBPos[0], DoorBPos[1], -1 * DoorOpenSpeed * Time.deltaTime);
+                }
+            }
+        }
+        else
+        {
+            if (DoorA.transform.position.z > DoorAPos[2])
+            {
+                if (DoorA.transform.position.z < DoorBPos[2])
+                {
+                    // For now do nothing
+                }
+            }
+        }
     }
 
-    /* Create a function that inputs start and end positions of doors, converts
-     value on a fraction and increments the door's position using a Sine function
-     */
-    public void ActivateDoor(float StartPos, float Position, float Displacement)
+    // Function checks if the player is at the sensor
+    private void SensorOnTrigger(Collider Object)
     {
-        for (float i = 1; i <= 500; i++)
+        if (Object.gameObject.tag == "Player")
         {
-            float Fraction = 1 - 1f / i;
-            //Position += Fraction * Mathf()
+            PlayerDetected = true;
+        }
+    }
+
+    // Function check if the player is not at the sensor
+    private void SensorOffTrigger(Collider Object)
+    {
+        if (Object.gameObject.tag != "Player")
+        {
+            PlayerDetected = false;
         }
     }
 }
