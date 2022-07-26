78c8481bf5c0cf36e793eb35cf8888f3421095e5
DamithTennakoon <88393680+DamithTennakoon@users.noreply.github.com>
2022-07-26 00:01:03 -0400

    Main Spawn Room Re-designed

--- Assets/C# Scripts/Effects/OffsetRotate.cs
+++ Assets/C# Scripts/Effects/OffsetRotate.cs
@@ -2,12 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Goal: Make objects rotate about the y-axis (purpose of effects)
-
-public class RotateObject : MonoBehaviour
+public class OffsetRotate : MonoBehaviour
 {
     // Define float variable to set speed of rotation
-    public float RotationSpeed = 30.0f;
+    public float RotationSpeed = 50.0f;
 
     // Define Game Object to be rotated
     public GameObject TargetObject;
@@ -15,12 +13,12 @@ public class RotateObject : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        TargetObject.transform.Rotate(Vector3.up * Time.deltaTime * RotationSpeed);
+        TargetObject.transform.Rotate(Vector3.forward * Time.deltaTime * RotationSpeed);
     }
 }
